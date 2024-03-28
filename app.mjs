222796495957b5aaafcedbcdcd13c65eb467adf0
bryanvengwa <bmvengwa@gmail.com>
2024-03-28 11:48:01 +0200

    added routes and controllers folders

--- app.mjs
+++ app.mjs
@@ -5,6 +5,9 @@ const app = express();
 app.get('/', (request, response) => {
     response.send('hello world');
 });
+app.get('/blog', (request, response) => {
+    response.send('hello Blog');
+});
 
 
 export default app;
