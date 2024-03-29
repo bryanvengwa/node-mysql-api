ed67d2e53eaaf63c148a6065d3757a4cb9f6fd20
bryanvengwa <bmvengwa@gmail.com>
2024-03-29 23:27:33 +0200

    removed dummy methods from the post controller

--- controllers/post.controller.mjs
+++
@@ -1,9 +0,0 @@
-export function index(request, response) {
-  const posts = 'Posts string';
-  response.send(posts);
-}
-
-export function post(request, response) {
-  const posts = 'Posts string';
-  response.send(posts);
-}
