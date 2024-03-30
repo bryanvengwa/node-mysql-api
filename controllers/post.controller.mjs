eacd9745e15ec66c95a9ae227e5241e98aab748c
bryanvengwa <bmvengwa@gmail.com>
2024-03-30 11:06:51 +0200

    working post functionality

--- controllers/post.controller.mjs
+++ controllers/post.controller.mjs
@@ -1,22 +1,22 @@
-// import models from '../models';
+import models from '../models/index.js'; 
 
 export function save(request, response, next) {
   console.log(request.body);
-  // const post = {
-  //   title: request.body.title,
-  //   content: request.body.content,
-  //   imageUrl: request.body.image_url,
-  //   categoryId: request.body.category_id,
-  //   userId: 1,
-  // };
+  const post = {
+    title: request.body.title,
+    content: request.body.content,
+    imageUrl: request.body.image_url,
+    categoryId: request.body.category_id,
+    userId: 1,
+  };
   
-  // models.Post.create(post)
-  //   .then((result) => {
-  //     response
-  //       .status(201)
-  //       .send({ message: 'post created successfully', post: result });
-  //   })
-  //   .catch((err) => {
-  //     response.status(500).send({ message: 'Something went wrong', post: err });
-  //   });
+  models.Post.create(post)
+    .then((result) => {
+      response
+        .status(201)
+        .send({ message: 'post created successfully', post: result });
+    })
+    .catch((err) => {
+      response.status(500).send({ message: 'Something went wrong', post: err });
+    });
 }
